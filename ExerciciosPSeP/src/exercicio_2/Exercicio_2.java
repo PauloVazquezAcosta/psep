8656d3cc6a9a73f7c3d1f7500aba0b373dcdaaae
U-DESKTOP-7A5237S\Paulo <Paulo Vzquez Acosta>
2021-10-28 12:55:13 +0200

    Exercicio #2 feito, funcional e comentado con #1
    Feito o exercicio 2 do PDF de PSeP segundo o diagrama de burbullas,
    executado funcionalmente e comentado. Comentado tamén o #1 para
    completalo

--- ExerciciosPSeP/src/exercicio_2/Exercicio_2.java
+++ ExerciciosPSeP/src/exercicio_2/Exercicio_2.java
@@ -1,33 +1,37 @@
-package exercicio_1;
+package exercicio_2;
 
 import java.util.Scanner;
 
-public class Exercicio_1 extends Thread {
+import exercicio_1.Exercicio_1;
+
+public class Exercicio_2 extends Thread {
 	// Creamos as variables que terá a secuencia
-	private static int a, b, x, y, z;
+	private static int cuad, x, m1, a, m2, b, z, y, c;
 	// Creamos a operación, cada fío terá a súa para o switch de run()
 	private int operacion;
 
 	// Introducimos a operación a facer cando creemos o fío de cada exercicio
-	public Exercicio_1(int operacion) {
+	Exercicio_2(int operacion) {
 		this.operacion = operacion;
 	}
 
 	public static void main(String[] args) {
 		// Creamos o teclado
 		Scanner teclado = new Scanner(System.in);
-		
+
 		// Pedimos os valores
-		System.out.print("Introduce o valor de y: ");
-		y = teclado.nextInt();
+		System.out.print("Introduce o valor de x: ");
+		x = teclado.nextInt();
 		System.out.print("Introduce o valor de a: ");
 		a = teclado.nextInt();
 		System.out.print("Introduce o valor de b: ");
 		b = teclado.nextInt();
+		System.out.print("Introduce o valor de c: ");
+		c = teclado.nextInt();
 
-		// Creamos os dous fíos do exercicio 1 do PDF
-		Exercicio_1 h1 = new Exercicio_1(1);
-		Exercicio_1 h2 = new Exercicio_1(2);
+		// Creamos os dous fíos do exercicio 2 do PDF
+		Exercicio_2 h1 = new Exercicio_2(1);
+		Exercicio_2 h2 = new Exercicio_2(2);
 
 		// Iniciamos os fíos
 		h1.start();
@@ -38,13 +42,14 @@ public class Exercicio_1 extends Thread {
 			h1.join();
 			h2.join();
 		} catch (InterruptedException e) {
-		}
-		;
-
-		/*
-		 * while (h1.isAlive() || h2.isAlive()) { }
-		 */
-
+		};
+		
+		// Cando acaben os fíos, continuamos as operacións no main xa que son secuenciais
+		z = m1 + m2;
+		System.out.println("z vale: " + z);
+		y = z + c;
+		System.out.println("y vale: " + y);
+		
 		System.out.println("O programa terminou");
 	}
 
@@ -52,14 +57,14 @@ public class Exercicio_1 extends Thread {
 
 		switch (operacion) {
 		case 1: // Operacións do fío 1
-			x = y + 1;
-			System.out.println("x vale: " + x);
-			y = x + 2;
-			System.out.println("y vale: " + y);
+			cuad = x * x;
+			System.out.println("cuad vale: " + cuad);
+			m1 = a * cuad;
+			System.out.println("m1 vale: " + m1);
 			break;
 		case 2: // Operacións do fío 2
-			z = a + b;
-			System.out.println("z vale: " + z);
+			m2 = b * x;
+			System.out.println("m2 vale: " + m2);
 			break;
 		}
 	}
